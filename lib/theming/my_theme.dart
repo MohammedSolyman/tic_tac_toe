98992f9800559518e2098378310b57345f3be27e
MohammedSolyman <MohammedSolyman10@gmail.com>
2023-08-29 11:38:28 +0200

    first commit

---
+++ lib/theming/my_theme.dart
@@ -0,0 +1,39 @@
+
+
+
+// static const MaterialColor mcgpalette0 = MaterialColor(_mcgpalette0PrimaryValue, <int, Color>{
+//   50: Color(0xFFE4E7FA),
+//   100: Color(0xFFBBC3F2),
+//   200: Color(0xFF8D9BE9),
+//   300: Color(0xFF5F73E0),
+//   400: Color(0xFF3D55D9),
+//   500: Color(_mcgpalette0PrimaryValue),
+//   600: Color(0xFF1831CD),
+//   700: Color(0xFF142AC7),
+//   800: Color(0xFF1023C1),
+//   900: Color(0xFF0816B6),
+// });
+// static const int _mcgpalette0PrimaryValue = 0xFF1B37D2;
+
+
+
+
+
+
+
+
+
+
+// static const MaterialColor mcgpalette0 = MaterialColor(_mcgpalette0PrimaryValue, <int, Color>{
+//   50: Color(0xFFE0E1E4),
+//   100: Color(0xFFB3B4BB),
+//   200: Color(0xFF80828E),
+//   300: Color(0xFF4D4F60),
+//   400: Color(0xFF262A3E),
+//   500: Color(_mcgpalette0PrimaryValue),
+//   600: Color(0xFF000319),
+//   700: Color(0xFF000314),
+//   800: Color(0xFF000211),
+//   900: Color(0xFF000109),
+// });
+// static const int _mcgpalette0PrimaryValue = 0xFF00041C;
