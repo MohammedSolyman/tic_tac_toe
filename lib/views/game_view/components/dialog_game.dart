188f3110eed585214262357c9ba1271fffead90a
MohammedSolyman <MohammedSolyman10@gmail.com>
2024-01-10 18:57:28 +0200

    text of winning dialog was autosized

--- lib/views/game_view/components/dialog_game.dart
+++ lib/views/game_view/components/dialog_game.dart
@@ -7,94 +7,6 @@ import 'package:google_fonts/google_fonts.dart';
 dialogGame(BuildContext context) async {
   GameController controller = Get.put(GameController());
 
-  // Widget widget = Obx(() {
-  //   return Center(
-  //     child: Material(
-  //       child: Container(
-  //         width: MediaQuery.of(context).size.width * 0.75,
-  //         height: MediaQuery.of(context).size.height * 0.25,
-  //         decoration: BoxDecoration(
-  //             color: Theme.of(context).primaryColorLight,
-  //             border:
-  //                 Border.all(width: 3, color: Theme.of(context).primaryColor),
-  //             borderRadius: const BorderRadius.all(Radius.circular(15))),
-  //         child: Column(
-  //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //           children: [
-  //             Column(
-  //               children: [
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //                   children: [
-  //                     SizedBox(
-  //                       width: MediaQuery.of(context).size.width * 0.60,
-  //                       child: AutoSizeText('player 1 chooses ',
-  //                           maxLines: 1,
-  //                           minFontSize: 5,
-  //                           style: Theme.of(context).textTheme.bodySmall),
-  //                     ),
-  //                     controller.gameViewModel.value.player1chooseX
-  //                         ? SmallX(
-  //                             width: MediaQuery.of(context).size.width * 0.15)
-  //                         : SmallO(
-  //                             width: MediaQuery.of(context).size.width * 0.15)
-  //                   ],
-  //                 ),
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //                   children: [
-  //                     SizedBox(
-  //                       width: MediaQuery.of(context).size.width * 0.60,
-  //                       child: AutoSizeText('player 2 chooses ',
-  //                           maxLines: 1,
-  //                           minFontSize: 5,
-  //                           style: Theme.of(context).textTheme.bodySmall),
-  //                     ),
-  //                     controller.gameViewModel.value.player1chooseX
-  //                         ? SmallO(
-  //                             width: MediaQuery.of(context).size.width * 0.15)
-  //                         : SmallX(
-  //                             width: MediaQuery.of(context).size.width * 0.15)
-  //                   ],
-  //                 ),
-  //               ],
-  //             ),
-  //             Row(
-  //               mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //               children: [
-  //                 InkWell(
-  //                   onTap: () {
-  //                     controller.shuffleXO();
-  //                   },
-  //                   child: AutoSizeText(
-  //                     'shuffle',
-  //                     maxLines: 1,
-  //                     minFontSize: 5,
-  //                     style: Theme.of(context).textTheme.bodySmall,
-  //                   ),
-  //                 ),
-  //                 InkWell(
-  //                   onTap: () {
-  //                     controller.okFunction();
-  //                   },
-  //                   child: AutoSizeText(
-  //                     'ok',
-  //                     maxLines: 1,
-  //                     minFontSize: 5,
-  //                     style: Theme.of(context).textTheme.bodySmall,
-  //                   ),
-  //                 ),
-  //               ],
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // });
-
-  // await Get.dialog(widget);
-
   await Get.defaultDialog(
     backgroundColor: Theme.of(context).primaryColorLight,
     title: '',
@@ -107,7 +19,7 @@ dialogGame(BuildContext context) async {
               flex: 4,
               child: AutoSizeText('player 1 chooses ',
                   maxLines: 1,
-                  minFontSize: 5,
+                  minFontSize: 1,
                   style: Theme.of(context).textTheme.bodySmall),
             ),
             Obx(() {
@@ -117,7 +29,7 @@ dialogGame(BuildContext context) async {
                 child: Center(
                   child: AutoSizeText(symbol,
                       maxLines: 1,
-                      // minFontSize: 5,
+                      minFontSize: 1,
                       style: GoogleFonts.pressStart2p(
                           color: symbol == 'x' ? Colors.green : Colors.red)),
                 ),
@@ -132,7 +44,7 @@ dialogGame(BuildContext context) async {
               flex: 4,
               child: AutoSizeText('player 2 chooses ',
                   maxLines: 1,
-                  //  minFontSize: 5,
+                  minFontSize: 1,
                   style: Theme.of(context).textTheme.bodySmall),
             ),
             Obx(() {
@@ -142,7 +54,7 @@ dialogGame(BuildContext context) async {
                 child: Center(
                   child: AutoSizeText(symbol,
                       maxLines: 1,
-                      // minFontSize: 5,
+                      minFontSize: 1,
                       style: GoogleFonts.pressStart2p(
                           color: symbol == 'x' ? Colors.green : Colors.red)),
                 ),
@@ -170,29 +82,3 @@ dialogGame(BuildContext context) async {
     ],
   );
 }
-
-// class SmallX extends StatelessWidget {
-//   const SmallX({required this.width, super.key});
-//   final double width;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AutoSizeText('x',
-//         style: GoogleFonts.pressStart2p(
-//           color: Colors.green,
-//         ));
-//   }
-// }
-
-// class SmallO extends StatelessWidget {
-//   const SmallO({required this.width, super.key});
-//   final double width;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AutoSizeText('o',
-//         style: GoogleFonts.pressStart2p(
-//           color: Colors.red,
-//         ));
-//   }
-// }
--- lib/views/game_view/components/dialog_win.dart
+++ lib/views/game_view/components/dialog_win.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_toe/controllers/game_comtroller.dart';
@@ -9,21 +10,21 @@ dialogWin(BuildContext context) async {
     backgroundColor: Theme.of(context).primaryColorLight,
     title: '',
     barrierDismissible: false,
-    content: Column(
+    content: Row(
       children: [
-        Row(
-          children: [
-            Obx(() {
-              String statement = controller
-                          .gameViewModel.value.currentPlayer!.name ==
-                      ''
-                  ? 'player ${controller.gameViewModel.value.currentPlayer!.symbol} wins'
-                  : '${controller.gameViewModel.value.currentPlayer!.name} wins';
+        Expanded(
+          child: Obx(() {
+            String statement = controller
+                        .gameViewModel.value.currentPlayer!.name ==
+                    ''
+                ? 'player ${controller.gameViewModel.value.currentPlayer!.symbol} wins'
+                : '${controller.gameViewModel.value.currentPlayer!.name} wins';
 
-              return Text(statement,
-                  style: Theme.of(context).textTheme.bodySmall);
-            }),
-          ],
+            return AutoSizeText(statement,
+                maxLines: 1,
+                minFontSize: 1,
+                style: Theme.of(context).textTheme.bodySmall);
+          }),
         ),
       ],
     ),
